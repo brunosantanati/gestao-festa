7eb245ce73de7cc458207a118b274d8d2536604a
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2020-05-14 14:42:49 -0300

    Inserir convidados no BD e passar lista de convidados para view para iterar sobre ela.

--- src/main/java/com/algaworks/festa/controller/ConvidadosController.java
+++ src/main/java/com/algaworks/festa/controller/ConvidadosController.java
@@ -1,14 +1,26 @@
 package com.algaworks.festa.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.algaworks.festa.repository.Convidados;
 
 @Controller
 public class ConvidadosController {
 	
+	@Autowired
+	private Convidados convidados;
+	
 	@GetMapping("/convidados")
-	public String listar() {
-		return "ListaConvidados";
+	public ModelAndView listar() {
+		
+		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
+		
+		modelAndView.addObject("convidados", convidados.findAll());
+		
+		return modelAndView;
 	}
 
 }
