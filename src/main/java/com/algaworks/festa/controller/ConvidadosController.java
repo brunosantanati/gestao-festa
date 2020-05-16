b56fd353bf5ad5d3e794c21a5e6434732830f65f
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2020-05-16 18:25:07 -0300

    Colocada anotacao RequestMapping no controller.

--- src/main/java/com/algaworks/festa/controller/ConvidadosController.java
+++ src/main/java/com/algaworks/festa/controller/ConvidadosController.java
@@ -4,18 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.algaworks.festa.model.Convidado;
 import com.algaworks.festa.repository.Convidados;
 
 @Controller
+@RequestMapping("/convidados")
 public class ConvidadosController {
 	
 	@Autowired
 	private Convidados convidados;
 	
-	@GetMapping("/convidados")
+	@GetMapping
 	public ModelAndView listar() {
 		
 		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
@@ -27,7 +29,7 @@ public class ConvidadosController {
 		return modelAndView;
 	}
 	
-	@PostMapping("/convidados")
+	@PostMapping
 	public String salvar(Convidado convidado) {
 		this.convidados.save(convidado);
 		return "redirect:/convidados";
